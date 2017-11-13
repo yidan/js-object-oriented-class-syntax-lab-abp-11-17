f97eacadc6b62a5bfc569d1e02ca9d6a31b53526
Yidan Wang <wangyidan621@gmail.com>
2017-11-13 01:23:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+class BoardMember{
+  constructor(name, homeState, training){
+    this.name = name
+    this.homeState = homeState
+    this.training = training
+  }
+  veto(){
+    return "No, I must disagree"
+  }
+
+  approve(){
+    return "You can do that!"
+  }
+
+  doCharity(){
+    return "I like to help people."
+  }
+
+  releasePressStatement(){
+    return "You will see great things from Scuber."
+  }
+
+  sayHi(){
+    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
+  }
+}
